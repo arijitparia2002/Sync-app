172934c338fe7a6429c079f7546a00c1706aef12
Arijit Paria <arijitparia2020@gmail.com>
2024-04-10 22:51:27 +0530

    basic component in all the pages

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Upcoming = () => {
+const Previous = () => {
   return (
-    <div>Upcoming</div>
+    <section className=" flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Previous</h1>
+    </section>
   )
 }
 
-export default Upcoming
+export default Previous;
