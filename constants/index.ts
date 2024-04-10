a775c2ff328df27e2dfcc8d0e9af249353ce96a3
Arijit Paria <arijitparia2020@gmail.com>
2024-04-10 20:50:04 +0530

    folder structure + layout + sidebar setup

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const SideBarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    icon: "/icons/home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    icon: "/icons/upcoming.svg",
+  },
+  {
+    label: "Popular",
+    route: "/popular",
+    icon: "/icons/popular.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    icon: "/icons/recordings.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    icon: "/icons/personal-room.svg",
+  },
+];
