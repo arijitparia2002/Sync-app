fbb6d4902be8b0fe15951787d7d2061fd09a529c
Arijit Paria <arijitparia2020@gmail.com>
2024-04-11 01:25:50 +0530

    navbar + mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="logo icon"
+          className="max-sm:size-10"
+        />
+        <p className="text-2xl text-center text-white font-extrabold">Sync</p>
+      </Link>
+      <div className="flex justify-between gap-5">
+        {/* clerk user management */}
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
